656c8d112c0046b7505b644454436550ac4ceca4
Mathias Amnell <mathias@apping.se>
2015-11-04 16:59:18 +0100

    Added Backdrop and some sprites

--- HackHunt/GameScene.swift
+++ HackHunt/GameScene.swift
@@ -1,7 +1,6 @@
 //
 //  GameScene.swift
-//  HackHunt
-//
+//  HackHuntUIColor(red:0.21, green:0.75, blue:1, alpha:1)//
 //  Created by Marcus Isaksson on 04/11/15.
 //  Copyright (c) 2015 HackHunt. All rights reserved.
 //
@@ -9,14 +8,17 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
         
-        self.addChild(myLabel)
+        self.backgroundColor = UIColor(red:0.21, green:0.75, blue:1, alpha:1)
+        
+        let backdrop = SKSpriteNode(imageNamed: "Backdrop")
+        backdrop.texture?.filteringMode = .Nearest
+        backdrop.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMinY(self.frame)+(backdrop.frame.size.height * 2) - 40);
+        backdrop.xScale = 2
+        backdrop.yScale = 2
+        self.addChild(backdrop)
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
